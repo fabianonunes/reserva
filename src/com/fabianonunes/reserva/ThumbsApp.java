8d6388a14e64e9b0928e362af2490a210604fee3
Fabiano Nunes <fabiano@nunes.me>
2011-06-19 11:58:07 -0300

    image-keys

--- src/com/fabianonunes/reserva/ThumbsApp.java
+++ src/com/fabianonunes/reserva/ThumbsApp.java
@@ -3,40 +3,19 @@
  */
 package com.fabianonunes.reserva;
 
-import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
-import java.awt.image.RenderedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-
-import javax.media.jai.Histogram;
-import javax.media.jai.Interpolation;
-import javax.media.jai.JAI;
-import javax.media.jai.RenderedOp;
-import javax.media.jai.operator.CropDescriptor;
-import javax.media.jai.operator.RotateDescriptor;
-import javax.media.jai.operator.ScaleDescriptor;
-import javax.media.jai.operator.SubsampleBinaryToGrayDescriptor;
-import javax.naming.spi.ObjectFactory;
-import javax.swing.JFileChooser;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
-import javax.xml.ws.Action;
 
 import org.apache.commons.io.FilenameUtils;
 
 import com.fabianonunes.reserva.model.PageAttributes;
-import com.fabianonunes.reserva.model.PageImage;
 import com.fabianonunes.reserva.pdf.PageProcessor;
 import com.fabianonunes.reserva.pdf.PdfImageUtils;
 import com.fabianonunes.reserva.pdf.PdfPageIterator;
@@ -45,129 +24,35 @@ import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.pdf.PdfCopy;
 import com.itextpdf.text.pdf.PdfDictionary;
 import com.itextpdf.text.pdf.PdfImportedPage;
-import com.itextpdf.text.pdf.PdfName;
-import com.itextpdf.text.pdf.PdfObject;
 import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.PdfStamper;
-import com.itextpdf.text.pdf.SimpleBookmark;
-import com.sun.jmx.snmp.tasks.Task;
 
-/**
- * The main class of the application.
- */
-public class ThumbsApp extends SingleFrameApplication {
+public class ThumbsApp {
 
-	private ThumbsView view;
-	private boolean runned = false;
-	ArrayList<Integer> lightPages;
+	Collection<Integer> lightPages;
 	Map<Integer, PageAttributes> attributes;
-	List<Amostra> amostras;
 	private PdfReader reader;
 	File file;
 
-	@Override
-	protected void startup() {
-		setView(new ThumbsView(this));
-		show(getView());
-
-	}
-
-	public static ThumbsApp getApplication() {
-		return Application.getInstance(ThumbsApp.class);
-	}
-
 	public static void main(String[] args) {
-		launch(ThumbsApp.class, args);
 	}
 
-	private void setView(ThumbsView thumbsView) {
-		this.view = thumbsView;
-	}
+	public void run() throws FileNotFoundException, DocumentException,
+			IOException {
 
-	private ThumbsView getView() {
-		return this.view;
-	}
+		doInBackground();
 
-	@Action(block = BlockingScope.WINDOW)
-	public Task<Object, PageImage> runThumbs() throws IOException,
-			DocumentException {
-
-		if (!runned) {
-			file = null;
-			runned = true;
-			return new RunThumbsTask(
-					org.jdesktop.application.Application
-							.getInstance(com.fabianonunes.solar.thumbs.ThumbsApp.class));
-		}
-
-		amostras = new ArrayList<Amostra>();
-
-		ArrayList<String> pagesToKeepInString = getView().grid.getNames();
 		ArrayList<Integer> pagesToKeep = new ArrayList<Integer>();
-
-		for (String name : pagesToKeepInString) {
-			pagesToKeep.add(Integer.parseInt(name));
-		}
-
-		int numberOfPages = reader.getNumberOfPages();
 
 		ArrayList<Integer> pagesToRemove = new ArrayList<Integer>();
 
-		for (int i = 1; i <= numberOfPages; i++) {
+		int numberOfPages = reader.getNumberOfPages();
 
-			PageAttributes att = attributes.get(i);
+		for (int i = 1; i <= numberOfPages; i++) {
 
 			reader.getPageContent(i);
 
-			Amostra amostra = new Amostra();
-
-			if (att != null) {
-
-				amostra.setBottom(BigDecimal.valueOf(att.getBottomKey()));
-				amostra.setMiddle(BigDecimal.valueOf(att.getMiddleKey()));
-				amostra.setTop(BigDecimal.valueOf(att.getTopKey()));
-				amostra.setContent(att.getTextInPage());
-
-			}
-
-			if (pagesToKeep.contains(i)) {
-				amostra.setRemoved(false);
-				amostras.add(amostra);
-				continue;
-			}
-
-			if (!lightPages.contains(i)) {
-				pagesToKeep.add(i);
-				amostra.setRemoved(false);
-				amostras.add(amostra);
-				continue;
-			}
-
 			pagesToRemove.add(i);
-			amostra.setRemoved(true);
-			amostras.add(amostra);
-
-		}
-
-		List<HashMap<String, Object>> bookmarks = SimpleBookmark
-				.getBookmark(reader);
-
-		if (bookmarks != null) {
-
-			for (HashMap<String, Object> hashMap : bookmarks) {
-
-				String pageAtt = (String) hashMap.get("Page");
-				if (pageAtt == null) {
-					continue;
-				}
-
-				pageAtt = pageAtt.substring(0, pageAtt.indexOf(" ")).trim();
-
-				Integer page = Integer.parseInt(pageAtt);
-				if (!pagesToKeep.contains(page))
-					pagesToKeep.add(page);
-
-			}
 
 		}
 
@@ -196,9 +81,9 @@ public class ThumbsApp extends SingleFrameApplication {
 		document.close();
 
 		Collections.sort(pagesToKeep);
-		
+
 		reader.selectPages(pagesToKeep);
-		
+
 		PdfStamper stamp = new PdfStamper(reader, new FileOutputStream(
 				buildOutputFilename("[L]")));
 
@@ -206,58 +91,111 @@ public class ThumbsApp extends SingleFrameApplication {
 
 		reader.close();
 
-		try {
-			createStats();
-		} catch (JAXBException e) {
-			e.printStackTrace();
-		}
-
-		exit();
-
-		return null;
 	}
 
-	private void createStats() throws JAXBException, FileNotFoundException {
+	protected Object doInBackground() throws IOException {
 
-		File currentDir = new File(System.getProperty("user.dir"));
-		String fileName = "stats.xml";
-		File outFile = new File(currentDir, fileName);
+		lightPages = Collections
+				.synchronizedCollection(new ArrayList<Integer>());
 
-		System.out.println(outFile);
-
-		Absoluto authors;
-		String pkgName = Absoluto.class.getPackage().getName();
-		JAXBContext jc = JAXBContext.newInstance(pkgName);
-
-		try {
-
-			Unmarshaller u = jc.createUnmarshaller();
-			authors = (Absoluto) u.unmarshal(outFile);
-
-		} catch (Exception e) {
-
-			ObjectFactory oFactory = new ObjectFactory();
-
-			authors = oFactory.createAbsoluto();
-
+		if (file == null) {
+			return null;
 		}
 
-		List<Amostra> currentAmostras = authors.getAmostra();
+		reader = new PdfReader(file.getAbsolutePath());
 
-		for (Amostra amostra : amostras) {
+		PdfPageIterator<Object> iterator = new PdfPageIterator<Object>(reader);
 
-			currentAmostras.add(amostra);
+		PageProcessor<Object> processor = new PageProcessor<Object>() {
 
-		}
+			private int counter = 0;
 
-		Marshaller marshaller;
+			@Override
+			public Object process(PdfDictionary page, Integer pageNumber)
+					throws Throwable {
 
-		marshaller = jc.createMarshaller();
+				counter++;
 
-		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, new Boolean(
-				true));
+				BufferedImage imageOfPage = PdfImageUtils
+						.extractImageFromPage(page);
 
-		marshaller.marshal(authors, new FileOutputStream(outFile));
+				ImageKeys keys = ImageKeys.normalize(imageOfPage);
+
+				Double key = keys.getKey(0.10f);
+				Double middleKey = keys.getMiddleKey(0.08f);
+				Double topKey = keys.getTopKey(0.08f);
+				Double bottomKey = keys.getBottomKey(0.08f);
+
+				Boolean isLightPage = false;
+
+				if (topKey <= 8d
+						&& (key < 15d || middleKey < 30d || bottomKey < 30)) {
+					isLightPage = true;
+				}
+
+				if (topKey <= 8d && middleKey < 8d) {
+					isLightPage = true;
+				}
+
+				if (key <= 19.8d && topKey <= 17) {
+
+					isLightPage = true;
+
+				}
+
+				if (key > 19.8d && key <= 21d) {
+
+					if (topKey <= 18d) {
+						isLightPage = true;
+					}
+
+				} else if (key < 26d) {
+
+					if (bottomKey + topKey < 31d) {
+						isLightPage = true;
+					}
+
+				}
+
+				if (topKey + middleKey + bottomKey > 95) {
+
+					isLightPage = false;
+
+				}
+
+				if (isLightPage) {
+
+					lightPages.add(pageNumber);
+
+				}
+
+				return null;
+
+			}
+
+			@Override
+			public void stop() {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public Boolean isStopped() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public void setProgress(Float progress) {
+				// TODO Auto-generated method stub
+
+			}
+
+		};
+
+		iterator.iterate(processor);
+
+		return null;
 
 	}
 
@@ -281,334 +219,4 @@ public class ThumbsApp extends SingleFrameApplication {
 		return "";
 	}
 
-	public File selectFile() {
-
-		FileSelectorDialog c = new FileSelectorDialog(getApplication()
-				.getMainFrame(), true);
-
-		JFileChooser fileChooser = c.getJFileChooser();
-		fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-		fileChooser.setMultiSelectionEnabled(false);
-
-		int rv = fileChooser.showOpenDialog(getApplication().getMainFrame());
-
-		if (rv == JFileChooser.APPROVE_OPTION) {
-
-			return fileChooser.getSelectedFile();
-
-		}
-
-		return null;
-
-	}
-
-	private class RunThumbsTask extends
-			org.jdesktop.application.Task<Object, PageImage> {
-
-		RunThumbsTask(org.jdesktop.application.Application app)
-				throws IOException {
-
-			super(app);
-
-			lightPages = new ArrayList<Integer>();
-
-			attributes = new HashMap<Integer, PageAttributes>();
-
-			file = selectFile();
-
-		}
-
-		@Override
-		protected void process(List<PageImage> values) {
-			super.process(values);
-
-			for (PageImage pageImage : values) {
-
-				JCell<Double> cell = getView().grid.getCell(pageImage,
-						getView().grid.cells.size() + 1);
-				cell.setComparable(pageImage.getStandardDeviation());
-
-				getView().grid.addCell(cell);
-
-			}
-		}
-
-		protected Object doInBackground() {
-
-			if (file == null) {
-				return null;
-			}
-
-			try {
-
-				reader = new PdfReader(file.getAbsolutePath());
-
-				PdfPageIterator<Object> iterator = new PdfPageIterator<Object>(
-						reader);
-
-				PageProcessor<Object> processor = new PageProcessor<Object>() {
-
-					private int counter = 0;
-					private boolean stop = false;
-
-					@Override
-					public Object process(PdfDictionary page, Integer pageNumber)
-							throws Throwable {
-
-						counter++;
-
-						BufferedImage imageOfPage = PdfImageUtils
-								.extractImageFromPage(page);
-
-						int width = imageOfPage.getWidth();
-						int height = imageOfPage.getHeight();
-
-						RenderedImage rop = imageOfPage;
-
-						Interpolation interpolation = Interpolation
-								.getInstance(Interpolation.INTERP_BICUBIC_2);
-
-						RenderingHints hints = new RenderingHints(
-								JAI.KEY_INTERPOLATION, interpolation);
-
-						float scale = (float) 350
-								/ Math.max(imageOfPage.getHeight(),
-										imageOfPage.getWidth());
-
-						if (width > height) {
-
-							float factor = 1.2f;
-
-							RenderedOp irop = RotateDescriptor
-									.create(rop,
-											0f,
-											0f,
-											(float) Math.toRadians(90f),
-											Interpolation
-													.getInstance(Interpolation.INTERP_BICUBIC_2),
-											null, null);
-							irop = ScaleDescriptor.create(irop, scale * factor,
-									scale * factor, 0f, 0f, null, null);
-							irop = SubsampleBinaryToGrayDescriptor.create(
-									irop.getAsBufferedImage(), 1 / factor,
-									1 / factor, hints);
-							rop = irop;
-						} else {
-							rop = SubsampleBinaryToGrayDescriptor.create(rop,
-									scale, scale, hints);
-						}
-
-						Double key = getKey(0.10f, rop);
-						Double middleKey = getMiddleKey(0.08f, rop);
-						Double topKey = getTopKey(0.08f, rop);
-						Double bottomKey = getBottomKey(0.08f, rop);
-
-						PdfObject contents = PdfReader.getPdfObject(page
-								.get(PdfName.CONTENTS));
-
-						String textInPage;
-
-						textInPage = PdfTools.getTextFromObject(contents);
-
-						PageAttributes pa = new PageAttributes(pageNumber, key,
-								bottomKey, topKey, middleKey, textInPage);
-
-						attributes.put(pageNumber, pa);
-
-						Boolean test = false;
-
-						if (topKey <= 8d
-								&& (key < 15d || middleKey < 30d || bottomKey < 30)) {
-							test = true;
-						}
-
-						if (topKey <= 8d && middleKey < 8d) {
-							test = true;
-						}
-
-						if (key <= 19.8d && topKey <= 17) {
-
-							test = true;
-
-						}
-
-						if (key > 19.8d && key <= 21d) {
-
-							if (topKey <= 18d) {
-								test = true;
-							}
-
-						} else if (key < 26d) {
-
-							if (bottomKey + topKey < 31d) {
-								test = true;
-							}
-
-						}
-
-						if (topKey + middleKey + bottomKey > 95) {
-
-							test = false;
-
-						}
-
-						if (test) {
-
-							PageImage pi = new PageImage();
-							pi.setImage(((RenderedOp) rop).getAsBufferedImage());
-							pi.setPageNumber(pageNumber);
-							pi.setStandardDeviation(key);
-
-							publish(pi);
-
-							lightPages.add(pageNumber);
-
-						} else {
-
-						}
-
-						return null;
-
-					}
-
-					private Double getMiddleKey(float margin, RenderedImage rop) {
-
-						float pageWidth = rop.getWidth();
-
-						float pageHeight = rop.getHeight();
-
-						margin = margin * Math.min(pageWidth, pageHeight);
-
-						float x = margin * 2;
-						float width = pageWidth - 2 * x;
-
-						float height = pageHeight * 0.20f;
-						float y = margin + height;
-
-						RenderedOp topCropped = CropDescriptor.create(rop, x,
-								y, width, height, null);
-
-						Histogram topHistogram = (Histogram) JAI.create(
-								"histogram", topCropped).getProperty(
-								"histogram");
-
-						return topHistogram.getStandardDeviation()[0];
-					}
-
-					private Double getKey(float margin, RenderedImage rop) {
-
-						margin = margin
-								* Math.min(rop.getWidth(), rop.getHeight());
-
-						float x = margin * 2;
-						float y = margin;
-						float width = rop.getWidth() - margin * 4;
-						float height = rop.getHeight() - margin * 2;
-
-						RenderedOp croppped = CropDescriptor.create(rop, x, y,
-								width, height, null);
-
-						Histogram histogram = (Histogram) JAI.create(
-								"histogram", croppped).getProperty("histogram");
-
-						return histogram.getStandardDeviation()[0];
-
-					}
-
-					private Double getTopKey(float margin, RenderedImage rop) {
-
-						float pageWidth = rop.getWidth();
-
-						float pageHeight = rop.getHeight();
-
-						margin = margin * Math.min(pageWidth, pageHeight);
-
-						float x = margin * 2;
-						float width = pageWidth - 2 * x;
-
-						float y = margin;
-						float height = pageHeight * 0.20f;
-
-						RenderedOp topCropped = CropDescriptor.create(rop, x,
-								y, width, height, null);
-
-						Histogram topHistogram = (Histogram) JAI.create(
-								"histogram", topCropped).getProperty(
-								"histogram");
-
-						return topHistogram.getStandardDeviation()[0];
-
-					}
-
-					private Double getBottomKey(Float margin, RenderedImage rop) {
-
-						float pageWidth = rop.getWidth();
-						float x = pageWidth * margin * 2;
-						float width = pageWidth - 2 * x;
-
-						float pageHeight = rop.getHeight();
-						float y = (float) (2 * pageHeight / 3);
-						float height = (pageHeight / 3) - pageHeight * margin;
-
-						RenderedOp bottomCropped = CropDescriptor.create(rop,
-								x, y, width, height, null);
-
-						Histogram bottomHistogram = (Histogram) JAI.create(
-								"histogram", bottomCropped).getProperty(
-								"histogram");
-
-						return bottomHistogram.getStandardDeviation()[0];
-
-					}
-
-					@Override
-					public void stop() {
-						stop = true;
-					}
-
-					@Override
-					public void setProgress(Float progress) {
-						RunThumbsTask.this.setProgress(progress / 100);
-					}
-
-					@Override
-					public Boolean isStopped() {
-						return stop;
-					}
-
-				};
-
-				iterator.iterate(processor);
-
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-
-			return null;
-
-		}
-
-		@Override
-		protected void finished() {
-			super.finished();
-			System.gc();
-		}
-
-		@Override
-		protected void succeeded(Object result) {
-
-			int childrenLenght = getView().grid.contentPanel
-					.getComponentCount();
-
-			for (int i = 0; i < childrenLenght; i++) {
-
-				JCell<?> cell = (JCell<?>) getView().grid.contentPanel
-						.getComponent(i);
-				cell.setIndex(i);
-
-			}
-
-		}
-
-	}
 }
