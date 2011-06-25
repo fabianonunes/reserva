1d99c37d52d5c7e729da3cb0de0061c4f9c4d850
Fabiano Nunes <fabiano@nunes.me>
2011-06-25 17:59:43 -0300

    test mods

--- src/test/java/com/fabianonunes/reserva/StoreTest.java
+++ src/test/java/com/fabianonunes/reserva/StoreTest.java
@@ -1,7 +1,12 @@
 package com.fabianonunes.reserva;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.text.Normalizer;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -22,7 +27,19 @@ public class StoreTest {
 
 	@Test
 	public void testSplit() throws Exception {
-		s.split();
+		// s.split();
+
+		File file = new File("/home/fabiano/Desktop/untitled");
+		File output = new File("/home/fabiano/Desktop/output");
+
+		String reference = FileUtils.readFileToString(file);
+
+		reference = Normalizer.normalize(reference, Normalizer.Form.NFD);
+		reference = reference.replaceAll("[^\\p{L}\\s]", "");
+		
+//		System.out.println(reference);
+
+		IOUtils.write(reference, new FileOutputStream(output));
 	}
 
 }
